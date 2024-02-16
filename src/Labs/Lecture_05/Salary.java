2ac30d085498d19100f6a62f29b84580ce5e42c6
ArnavKucheriya <ArnavKucheriya@outlook.com>
2024-02-16 10:22:35 -0500

    Linting

--- src/Labs/Lecture_05/Salary.java
+++ src/Labs/Lecture_05/Salary.java
@@ -15,22 +15,27 @@ import java.text.NumberFormat;
 
 public class Salary {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         double currentSalary; // employee's current salary
         double raise; // amount of the raise
         double newSalary; // new salary for the employee
         String rating; // performance rating
-        Scanner scan = new Scanner(System.in);
+
         System.out.print("Enter the current salary: ");
         currentSalary = scan.nextDouble();
         System.out.print("Enter the performance rating (Excellent, Good, or Poor): ");
         rating = scan.next();
+
         if(rating.equalsIgnoreCase("Excellent"))
             raise = currentSalary * 0.06;
         else if(rating.equalsIgnoreCase("Good"))
             raise = currentSalary * 0.04;
         else
             raise = currentSalary * 0.015;
+
         newSalary = currentSalary + raise;
+
         // Print the results
         NumberFormat money = NumberFormat.getCurrencyInstance();
         System.out.println();
